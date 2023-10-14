b00ddbe8b3cfa690ba0e5958f3e3114d5692ca28
Cepa95 <ceprnic.josip@gmail.com>
2023-10-14 22:24:32 +0200

    Maven/lombok

---
+++ task02/src/main/java/com/oss/jceprnic/Menu.java
@@ -0,0 +1,14 @@
+package com.oss.jceprnic;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class Menu {
+
+    private String name;
+    public Menu(){
+        this.name = "Josip";
+    }
+
+}
---
+++ task02/src/main/java/com/oss/jceprnic/Sensor.java
@@ -0,0 +1,9 @@
+package com.oss.jceprnic;
+
+public class Sensor {
+    public static void main(String[] args){
+        Menu menu = new Menu();
+        System.out.println(menu.getName());
+
+    }
+}
